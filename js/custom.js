9367d671721de3b8d70e7519e4516f7a5dabcdbc
slychops <richies75@gmail.com>
2017-01-18 11:19:01 +0100

    some changes

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,7 @@
 console.log("---")
+var currentImage;
+var intMod = 0;
+var intWidth;
 
 $('#question1').click(function() {
   				$('#answer1').toggle("fast", function(){
@@ -8,4 +11,37 @@ $('#question1').click(function() {
 
 $('.readmore').click(function() {
 	$('.morecontent').toggle("fast");
+})
+
+
+$(".panel").click(function(e) {
+	currentImage = e.target;
+	
+	if (intWidth != "70%") {
+		$(currentImage)
+			.css("position", "fixed")
+			.css("z-index","1")
+			// intWidth=currentImage.clientwidth
+			var intWidth = $(currentImage).width()
+
+			.animate({
+				width:"70%",
+				height:"100%",
+				margin:0,
+				top: 0,
+				left: "15%"
+			})
+	}  else {
+		$(currentImage)
+		.css("position", "fixed")
+		.animate({
+			width:"30%",
+			height:"100%",
+			margin:0,
+			top:0,
+			left: "5%"
+		})
+		}
+
+
 })
